4e33f3736be4522dbe71d4808a41af3d44804078
Hao Ngo <haoxray@gmail.com>
2019-07-21 08:52:32 +0700

    remove cors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 
 const jwt = require("jsonwebtoken");
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
 
 const App = express();
